e191155e3b5c2e26030093ee7ebd5b46ce4242be
gibsongreen <42986583+gibsongreen@users.noreply.github.com>
2018-11-23 18:48:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 alert('Hello JavaScript!');
-function openContent(event, About Me) {
-    var i, tabcontent, tablinks;
+function main() {
+ $('.tabcontent').hide();
+  $('.tabcontent').fadeIn(400)
+}
+$(document).ready(main);
 
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
 
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+//function openContent(event, About Me) {
+ //   var i, tabcontent, tablinks;
+
+   // tabcontent = document.getElementsByClassName("tabcontent");
+   // for (i = 0; i < tabcontent.length; i++) {
+   //     tabcontent[i].style.display = "none";
+  //  }
+
+   // tablinks = document.getElementsByClassName("tablinks");
+  //  for (i = 0; i < tablinks.length; i++) {
+       // tablinks[i].className = tablinks[i].className.replace(" active", "");
+  //  }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(About Me).style.display = "block";
-    evt.currentTarget.className += " active";
+   // document.getElementById(About Me).style.display = "block";
+    //evt.currentTarget.className += " active";
