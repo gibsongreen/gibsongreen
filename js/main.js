f60ee07f7b5b2d150111cf2c27c91b072351251c
gibsongreen <42986583+gibsongreen@users.noreply.github.com>
2018-11-23 18:02:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 alert('Hello JavaScript!');
+function openContent(event, About Me) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(About Me).style.display = "block";
+    evt.currentTarget.className += " active";
