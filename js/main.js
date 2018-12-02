3b67d337660e0e35d62929415adfb39fa4d452e0
gibsongreen <42986583+gibsongreen@users.noreply.github.com>
2018-12-01 19:23:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,87 @@
-alert('Hello JavaScript!');
-function main() {
- $('.tabcontent').hide();
-  $('.tabcontent').fadeIn(400)
+body {
+  background-color: lightgrey;
 }
-$(document).ready(main);
+.title {
+  color:teal;
+}
+#EducationalExperience {
+  border: 1px solid black;
+     padding: 10px;
+     width: 775px;
+     height: 515px;
+     text-align: justify;        
+}
+#AboutMe {
+  border: 1px solid black;
+     padding: 10px;
+     width: 639px;
+     height: 625px;
+     text-align: justify;
+ }
+ img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+#WorkSamples {
+  border: 1px solid black;
+     padding: 10px;
+     width: 457px;
+     height: 415px;
+     text-align: justify;
+} 
+#TopAccomplishments {
+    border: 1px solid black;
+       padding: 10px;
+       width: 535px;
+       height: 225px;
+       text-align: justify;
+}
+#OtherProfiles {
+  border: 1px solid black;
+     padding: 10px;
+     width: 550px;
+     height: 150px;
+     text-align: justify;
+}
 
+.tab {
+  overflow: hidden;
+  border: 1px solid black;
+  background-color: lightgrey;
+}
 
-//function openContent(event, About Me) {
- //   var i, tabcontent, tablinks;
+/* Style the buttons inside the tab */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+  font-size: 17px;
+}
 
-   // tabcontent = document.getElementsByClassName("tabcontent");
-   // for (i = 0; i < tabcontent.length; i++) {
-   //     tabcontent[i].style.display = "none";
-  //  }
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
 
-   // tablinks = document.getElementsByClassName("tablinks");
-  //  for (i = 0; i < tablinks.length; i++) {
-       // tablinks[i].className = tablinks[i].className.replace(" active", "");
-  //  }
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-   // document.getElementById(About Me).style.display = "block";
-    //evt.currentTarget.className += " active";
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+  animation: fadeEffect 1s; /* Fading effect takes 1 second */
+  /* Go from zero to full opacity */
+@keyframes fadeEffect {
+  from {opacity: 0;}
+  to {opacity: 1;}
+}
